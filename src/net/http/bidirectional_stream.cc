bb232d4ec8bae917e287e032e9a65a028434b4ff
klzgrad <kizdiv@gmail.com>
2022-05-22 01:31:43 +0800

    net, grpc_support: Set NetworkIsolationKey from header
    If BidirectionalStream request contains a -network-isolation-key
    header, it is used to set the network isolation key of the stream.
    The header itself is removed and not transmitted.
    
    The header value should be a valid URL with different host and port
    for each different network isolation key. Invalid header value is
    reported by returning error from bidirectional_stream_start.
    
    Network isolation takes effect only if it is enabled by experimental
    option of
    
      "feature_list": {
        "enable-features": "PartitionConnectionsByNetworkIsolationKey"
      }

--- src/net/http/bidirectional_stream.cc
+++ src/net/http/bidirectional_stream.cc
@@ -206,6 +206,13 @@ void BidirectionalStream::StartRequest() {
   HttpRequestInfo http_request_info;
   http_request_info.url = request_info_->url;
   http_request_info.method = request_info_->method;
+  if (auto network_isolation_key_header =
+          request_info_->extra_headers.GetHeader("-network-isolation-key")) {
+    request_info_->extra_headers.RemoveHeader("-network-isolation-key");
+    net::SchemefulSite site(GURL{*network_isolation_key_header});
+    CHECK(!site.opaque());
+    http_request_info.network_isolation_key = NetworkIsolationKey(site, site);
+  }
   http_request_info.extra_headers = request_info_->extra_headers;
   http_request_info.socket_tag = request_info_->socket_tag;
   stream_request_ =
