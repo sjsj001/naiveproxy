1d90e13c12651a01cc600d93be0b018bd98de8df
世界 <i@sekai.icu>
2025-12-13 16:39:24 +0800

    Fix iOS build

--- src/net/base/net_string_util_icu_alternatives_ios.mm
+++ src/net/base/net_string_util_icu_alternatives_ios.mm
@@ -76,7 +76,7 @@ bool ConvertToUTF16WithSubstitutions(std::string_view text,
   return false;
 }
 
-bool ToUpper(std::u16string_view str, std::u16string* output) {
+bool ToUpperUsingLocale(std::u16string_view str, std::u16string* output) {
   base::apple::ScopedCFTypeRef<CFStringRef> cfstring =
       base::SysUTF16ToCFStringRef(str);
   base::apple::ScopedCFTypeRef<CFMutableStringRef> mutable_cfstring(
